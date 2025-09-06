e5ae7f3be07cb4092d303da415f72297757f55f2
Gabriel Oliveira <155106088+gabriel-oligom@users.noreply.github.com>
2025-09-05 21:19:54 -0300

    Create Seat model

--- app/models.py
+++ app/models.py
@@ -17,8 +17,18 @@ class Event(Base):
     name = Column(String, nullable=False)
     total_seats = Column(Integer, nullable=False)
 
-# Represents a hierarchical relationship to the Seat class; 
-# access all seats belonging to this event 
+    # Represents a hierarchical relationship to the Seat class; 
+    # access all seats belonging to this event 
     seats = relationship("Seat", back_populates="event")
 
 
+class Seat(Base):
+    __tablename__ = "seats"
+
+    id = Column(Integer, primary_key=True, index=True)
+    number = Column(Integer, nullable=False)
+    status = Column(String, nullable=False, default="available")
+    event_id = Column(Integer, ForeignKey("events.id")) # 'event_id' is a foreign key that references the 'id' column in the 'events' table.
+
+    # Inverse relation to access Event
+    event = relationship("Event", back_populates="seats")
