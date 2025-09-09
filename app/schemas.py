0b9f8b86d6bb0ba389993c378f41bce9cac32d27
Gabriel Oliveira <155106088+gabriel-oligom@users.noreply.github.com>
2025-09-08 22:04:56 -0300

    Create Event schemas with Pydantic

---
+++ app/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+"""
+BaseModel : it is the base class of Pydantic, so all schemas inherit from it
+Field : is used to metadata, like 'title', 'example', and validation like 'ge' and 'le'
+Optional : we  use when a field can be 'None' 
+"""
+
+class EventBase(BaseModel):
+    name: str = Field(..., title="Event name", example="MTV Unplugged")
+    total_seats: int = Field(..., title="Total seats", ge=1, example=5)
+
+
+class EventCreate(EventBase):
+    pass
+
+
+class EventRead(EventBase):
+    id: int
+
+    class Config:
+        orm_mode = True
