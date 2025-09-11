e8693536678c94c8ee17f6eb78ab31ab015fb0ba
Gabriel Oliveira <155106088+gabriel-oligom@users.noreply.github.com>
2025-09-11 09:30:20 -0300

    Update EventCreate validation and clarify comments

--- app/schemas.py
+++ app/schemas.py
@@ -2,21 +2,23 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 """
-BaseModel : it is the base class of Pydantic, so all schemas inherit from it
+Pydantic: a module that validates and structures data before it's processed or stored in the database
+BaseModel : defines the structure and validation rules for data models. It's base class of Pydantic, so all schemas inherit from it
 Field : is used to metadata, like 'title', 'example', and validation like 'ge' and 'le'
-Optional : we  use when a field can be 'None' 
+Optional : we use when a field can be 'None', when is not mandatory
 """
 
 class EventBase(BaseModel):
-    # Common fields for Event schema
+    # Common fields/structure for Event schema
     name: str = Field(..., title="Event name", example="MTV Unplugged")
     total_seats: int = Field(..., title="Total seats", ge=1, example=5)
 
 
 class EventCreate(EventBase):
     # Schema for creating an event
+    total_seats: int = Field(..., ge=10, le=1000, title="Total seats", example=50)
     pass
-
+ 
 
 class EventRead(EventBase):
     # Schema for reading an event (includes ID)
