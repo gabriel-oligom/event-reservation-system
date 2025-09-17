5889e0b77cc13119f9536a8ac452ba174594d24e
Gabriel Oliveira <155106088+gabriel-oligom@users.noreply.github.com>
2025-09-16 22:26:07 -0300

    fix: include reservations router in main app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .database import SessionLocal, engine
-from .routers import events, seats
+from .routers import events, seats, reservations
 
 # Ensure models are registered and tables exist
 models.Base.metadata.create_all(bind=engine)
@@ -10,6 +10,7 @@ app = FastAPI() # Create the instance of the application
 
 app.include_router(events.router)
 app.include_router(seats.router)
+app.include_router(reservations.router)
 
 @app.get("/")
 def root():
